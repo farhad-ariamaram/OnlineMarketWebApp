95749e8d7175485d1025d9da819b9cd98767563e
ym <ym@DESKTOP-2HUH2VU>
2021-06-27 11:28:29 +0430

    initial

---
+++ OnlineMarketWebApp/Models/OnlineMarketDBContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace OnlineMarketWebApp.Models
+{
+    public partial class OnlineMarketDBContext : DbContext
+    {
+        public OnlineMarketDBContext()
+        {
+        }
+
+        public OnlineMarketDBContext(DbContextOptions<OnlineMarketDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=OnlineMarketDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.ToTable("Product");
+
+                entity.Property(e => e.Detail)
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ OnlineMarketWebApp/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace OnlineMarketWebApp.Models
+{
+    public partial class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public long Price { get; set; }
+    }
+}
