d8eb84aff276ae92da69c4021009dcd90a396b8f
Farhad Ariamaram <farhad.ariamaram@gmail.com>
2021-06-28 19:46:24 +0430

    init

---
+++ OnlineMarketWebApp/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿function quitBox() {
+	if (navigator.userAgent.indexOf("Firefox") != -1 || navigator.userAgent.indexOf("Chrome") != -1) {
+		open(location, '_self').close();
+		window.location.href = "about:blank";
+		window.close();
+	} else {
+		window.opener = null;
+		window.open("", "_self");
+		window.close();
+		open(location, '_self').close();
+	}  
+}
